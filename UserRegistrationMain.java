dfbf7e8fb46a8a6cab3621e563ce47b67716c7e0
vinit <vinitkkulkarni@gmail.com>
2023-04-30 10:20:29 +0530

    Added:inital welcom  msg

---
+++ UserRegistrationMain.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.userregistration;
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("USER REGISTRATION WITH REGEX");
+    }
+}
